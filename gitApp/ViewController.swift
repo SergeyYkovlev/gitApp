154d34ca2cef69e4263f5018bde5219859a034f7
start <yakovlevser09@gmail.com>
2021-12-21 19:00:39 +0600

    1

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Сергей Яковлев on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func showAlert() {
+        
+        print("Hello!")
+        
+    }
+
+}
+

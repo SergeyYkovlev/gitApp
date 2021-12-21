1f499784c36473437184169ed948b3588df1c644
start <yakovlevser09@gmail.com>
2021-12-21 19:13:48 +0600

    2

--- gitApp/ViewController.swift
+++ gitApp/ViewController.swift
@@ -17,8 +17,13 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         
-        print("Hello!")
+        let alert = UIAlertController(title: "Hello world", message: "This is ny first app!", preferredStyle: .alert)
         
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+     
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 
 }
